1921a4314b06160dde703de57fc8711b2d814743
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2019-11-27 11:54:59 +0100

    oops, leave old cornetto rule in

--- src/rewrite_lmf_synset_relations.pl
+++ src/rewrite_lmf_synset_relations.pl
@@ -3,7 +3,6 @@ internal_relation @@
  { Source, lmf:id, literal(SourceId) },
  { RList, lmf:synset_relation, Rel },
  { Rel, lmf:rel_type, literal(Name) },
-% { Rel, lmf:provenance, literal(Prov) } , % hard to convert without reification of the relation triple
  { Rel, lmf:target, literal(TargetId) }
  ==>
  irel_uri(Name, Pred),
@@ -11,7 +10,30 @@ internal_relation @@
  synset_uri(TargetId, Target_URI),
  { Source_URI, Pred, Target_URI } >> synset_relations.
 
-internal_relation_edoal @@
+internal_relation_edoal_cornetto @@
+{ Source, lmf:synset_relations, RList },
+{ Source, lmf:id, literal(SourceId) },
+{ RList, lmf:synset_relation, Rel },
+{ Rel,  lmf:meta, Meta },
+{ Meta, lmf:author, literal(Author) },
+{ Meta, lmf:date, literal(Date) },
+{ Meta, lmf:confidence, literal(Confidence) },
+{ Rel,  lmf:rel_type, literal(Name) },
+{ Rel,  lmf:target, literal(TargetId) }
+==>
+internal_relations_edoal(Alignment) ,
+synset_uri(SourceId, Source_URI),
+synset_uri(TargetId, Target_URI),
+rdf_bnode(Cell),
+{ Alignment, align:map, Cell } >> edoal_internal ,
+{ Cell, align:entity1, Source_URI } >> edoal_internal ,
+{ Cell, align:entity2, Target_URI } >> edoal_internal ,
+{ Cell, align:measure, Confidence^^xsd:integer } >> edoal_internal ,
+{ Cell, align:relation, literal(Name) } >> edoal_internal ,
+{ Cell, dc:creator, literal(Author) } >> edoal_internal ,
+{ Cell, dc:date, literal(Date) } >> edoal_internal .
+
+internal_relation_edoal_odwn @@
 { Source, lmf:synset_relations, RList },
 { Source, lmf:id, literal(SourceId) },
 { RList, lmf:synset_relation, Rel },
